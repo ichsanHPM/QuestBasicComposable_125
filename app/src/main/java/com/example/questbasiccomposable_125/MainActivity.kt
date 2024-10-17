20bc93529ee3cae4e1be8faf40f9a8e30b92a64e
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2024-10-17 13:09:04 +0700

    mengatur tampilan menjadi vertikal

--- app/src/main/java/com/example/questbasiccomposable_125/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_125/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_125.ui.theme.QuestBasicComposable_125Theme
@@ -31,6 +34,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicLayout(modifier: Modifier = Modifier){
+    Column (
+    ){
+
+    }
 
 }
 
